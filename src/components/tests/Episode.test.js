b6a8bb611bed97169326f71d3b6bb7a3aa01c27e
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-06-14 18:38:49 -0700

    assignment complete

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,23 +8,37 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "Test Summary",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id:1,
+    name: "",
+    season: 1,
+    number: 1,
+    summary: "",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+    render (<Episode episode={testEpisode}/>)
 });
 
 test("renders the summury test passed as prop", ()=>{
+    render (<Episode episode={testEpisode}/>)
+    const value = screen.queryByText("Test Summary");
+    expect(value).toBeTruthy();
+    expect(value).toBeInTheDocument();
+    expect(value).toHaveTextContent(/test summary/i);
     
 });
 
 test("renders default image when image is not defined", ()=>{
+    render (<Episode episode ={testEpisodeWithoutImage}/>);
+    const value =screen.queryByAltText(/stranger_things.png/i);
+    expect(value).toBeInTheDocument();
     
 })
 
